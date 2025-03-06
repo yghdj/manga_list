e7dec05a96725fb0fa82fe2f4fb41b10bceea49b
Luc Moussiegt <47942311+yghdj@users.noreply.github.com>
2025-03-06 17:49:35 +0100

    Add new() functions for created types

--- src/data/manga.rs
+++ src/data/manga.rs
@@ -1,18 +1,22 @@
-// represents a series
+use std::str::FromStr;
+
+use celes::Country;
+
+// XXX: multiple editions by series:
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
-pub struct Manga {
-    title: String,
-    author: Vec<Author>,
-    editors: Vec<Editor>,
-    volume_count: i16,
+pub struct Series {
+    pub title: String,
+    pub authors: Vec<Author>,
+    pub editors: Vec<Editor>,
+    pub volume_count: i16,
 }
 
-impl Manga {
-    pub fn new(title: &str) -> Manga {
-        Manga {
+impl Series {
+    pub fn new(title: &str) -> Series {
+        Series {
             title: String::from(title),
-            author: Vec::new(),
+            authors: Vec::new(),
             editors: Vec::new(),
             volume_count: 0,
         }
@@ -21,15 +25,36 @@ impl Manga {
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
-struct Author {
+pub struct Author {
     family_name: String,
     given_name: String,
+    nationality: celes::Country,
+}
+
+impl Author {
+    pub fn new(family_name: &str, given_name: &str, nationality: &str) -> Author {
+        Author {
+            family_name: String::from(family_name),
+            given_name: String::from(given_name),
+            nationality: Country::from_str(nationality).unwrap_or(Country::japan()),
+        }
+    }
 }
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
-struct Editor {
-    name: String,
-    creation_date: time::Date,
-    country: celes::Country,
+pub struct Editor {
+    pub name: String,
+    pub creation_year: i32,
+    pub country: Country,
+}
+
+impl Editor {
+    pub fn new(name: &str, creation_year: i32, country: &str) -> Editor {
+        Editor {
+            name: String::from(name),
+            creation_year,
+            country: Country::from_str(country).unwrap_or(Country::japan()),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use crate::data::manga::Manga;
+use data::manga::{Author, Editor};
+
+use crate::data::manga::Series;
 
 mod data;
 
 fn main() {
-    let new_series = Manga::new("Hello World");
+    let mut rave = Series::new("Rave Groove Adventure");
 
-    println!("{:?}", new_series);
+    rave.authors.push(Author::new("Mashima", "Hiro", "JP"));
+
+    rave.editors.push(Editor::new("Kōdansha", 1909, "JP"));
+    rave.editors.push(Editor::new("Glénat", 1969, "FR"));
+    rave.editors.push(Editor::new("TOKYOPOP", 1997, "US"));
+
+    rave.volume_count = 35;
+
+    println!("{:?}", rave);
 }
